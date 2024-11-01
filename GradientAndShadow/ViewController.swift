f1fdefc654de230a70fc0c30dd3dacf789162f6d
Andrew Gogol <andrewgogol777@gmail.com>
2024-11-01 23:37:10 +0300

    Update with gradient and shadow

--- GradientAndShadow/ViewController.swift
+++ GradientAndShadow/ViewController.swift
@@ -9,24 +9,43 @@ import UIKit
 
 class ViewController: UIViewController {
     let squareView = UIView()
-
+    let gradientLayer = CAGradientLayer()
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         
-        squareView.backgroundColor = .blue
-        view.addSubview(squareView)
+        squareView.backgroundColor = .clear
+        squareView.layer.cornerRadius = 20
         
-        squareView.autoresizingMask = []
+        gradientLayer.colors = [
+            UIColor.systemPink.cgColor,
+            UIColor.blue.cgColor
+        ]
+        
+        gradientLayer.startPoint = CGPoint(x: 0, y: 0)
+        gradientLayer.endPoint = CGPoint(x: 1, y: 1)
+        gradientLayer.cornerRadius = 20
+        
+        
+        view.addSubview(squareView)
     }
-
+    
     override func viewWillLayoutSubviews() {
         super.viewWillLayoutSubviews()
-        
+
         let squareSize: CGFloat = 100
         let xPosition: CGFloat = 100
         let yPosition: CGFloat = (view.bounds.height - squareSize) / 2
-
+        
         squareView.frame = CGRect(x: xPosition, y: yPosition, width: squareSize, height: squareSize)
+        
+        squareView.layer.shadowColor = UIColor.black.cgColor
+        squareView.layer.shadowOpacity = 0.7
+        squareView.layer.shadowOffset = CGSize(width: 3, height: 3)
+        squareView.layer.shadowRadius = 7
+        
+        gradientLayer.frame = squareView.bounds
+        squareView.layer.insertSublayer(gradientLayer, at: 0)
     }
 }
 

1ecaca23fd67bd4edd3adb4f7c753ddbed96a370
Andrew Gogol <andrewgogol777@gmail.com>
2024-11-01 21:46:20 +0300

    Setup rotating rect

--- GradientAndShadow/ViewController.swift
+++ GradientAndShadow/ViewController.swift
@@ -8,12 +8,26 @@
 import UIKit
 
 class ViewController: UIViewController {
+    let squareView = UIView()
 
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        
+        squareView.backgroundColor = .blue
+        view.addSubview(squareView)
+        
+        squareView.autoresizingMask = []
     }
 
+    override func viewWillLayoutSubviews() {
+        super.viewWillLayoutSubviews()
+        
+        let squareSize: CGFloat = 100
+        let xPosition: CGFloat = 100
+        let yPosition: CGFloat = (view.bounds.height - squareSize) / 2
 
+        squareView.frame = CGRect(x: xPosition, y: yPosition, width: squareSize, height: squareSize)
+    }
 }
+
 
